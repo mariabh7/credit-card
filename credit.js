09d82733cc8c1262cead26fac7403ff6df4c821e
maria <bouhrem.maria@gmail.com>
2024-12-24 23:48:02 +0100

    last changes for today 24/12

--- credit.js
+++ credit.js
@@ -1,18 +1,24 @@
 const input1 = document.getElementById("name");
 const cardn = document.getElementById("cardn");
 const namei = document.getElementById("nameo");
-const cardi = document.getElementById("cardo");
-
+const cvc = document.getElementById("cvc");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+const cmonth = document.getElementById("c-month");
 input1.addEventListener('input', function () {
     namei.textContent = input1.value;
     if (input1.value === '') {
         namei.textContent = "your name";
     }
 });
-
 cardn.addEventListener('input', function () {
-    cardi.textContent = cardn.value;
-    if (cardn.length > 3) {
-        cardi.textContent = "invalid";
+    cvc.textContent = cardn.value;
+    if (cardn.value.length > 3) {
+        cvc.textContent = "000";
+    }
+}); cmonth.addEventListener('input', function () {
+    month.textContent = cmonth.value;
+    if (cmonth.value.length > 2) {
+        month.textContent = '00';
     }
 });
