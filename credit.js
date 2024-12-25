34be8bec940fd7f6856d7ae9872382605de8522f
maria <bouhrem.maria@gmail.com>
2024-12-25 13:49:58 +0100

    new code update

--- credit.js
+++ credit.js
@@ -1,24 +1,27 @@
-const input1 = document.getElementById("name");
-const cardn = document.getElementById("cardn");
-const namei = document.getElementById("nameo");
-const cvc = document.getElementById("cvc");
-const month = document.getElementById("month");
-const year = document.getElementById("year");
-const cmonth = document.getElementById("c-month");
-input1.addEventListener('input', function () {
-    namei.textContent = input1.value;
-    if (input1.value === '') {
-        namei.textContent = "your name";
-    }
-});
-cardn.addEventListener('input', function () {
-    cvc.textContent = cardn.value;
-    if (cardn.value.length > 3) {
-        cvc.textContent = "000";
-    }
-}); cmonth.addEventListener('input', function () {
-    month.textContent = cmonth.value;
-    if (cmonth.value.length > 2) {
-        month.textContent = '00';
-    }
-});
+const userinfo = {
+    namei: document.getElementById("nameofuser"),
+    cardNumber: document.getElementById("usernumber"),
+    cardMonth: document.getElementById("c-month"),
+    cardYear: document.getElementById("c-year"),
+    cardCvc: document.getElementById("usercvc"),
+}
+const userInfoOut = {
+    cardNumber: document.getElementById("cardnumber"),
+    namei: document.getElementById("username"),
+    cardMonth: document.getElementById("month"),
+    cardYear: document.getElementById("year"),
+    cardCvc: document.getElementById("cvc"),
+}
+for (let i in userinfo) {
+    userinfo[i].addEventListener('input', function () {
+        for (let j in userInfoOut) {
+
+            if (i === j) {
+                userInfoOut[j].textContent = userinfo[i].value;
+                if (userinfo[i].value === '') {
+                    userInfoOut[j].textContent = "error";
+                }
+            }
+        }
+    });
+}
